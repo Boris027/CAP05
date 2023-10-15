6d938d456e79f2e4be3c9bfacd5d787becce8303
Boris027 <titankiller027@gmail.com>
2023-10-16 01:22:40 +0200

    subida 1 tema 5

---
+++ Tema 5/src/Ejercicio2.java
@@ -0,0 +1,13 @@
+public class Ejercicio2 {
+    public static void main(String[] args) throws Exception {
+        
+        int num=0;
+
+        while (num<=100){
+
+            System.out.println(num);
+            num=num+5;
+        }
+        
+    }
+}
---
+++ Tema 5/src/Ejercicio3.java
@@ -0,0 +1,12 @@
+public class Ejercicio3 {
+    public static void main(String[] args) throws Exception {
+        
+        int num=0;
+
+        do{
+            System.out.println(num);
+            num=num+5;
+        }while(num<=100);
+        
+    }
+}
